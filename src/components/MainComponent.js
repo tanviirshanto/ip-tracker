5489462c4ec75269a908e14a414f8b9796dd0bf9
Shannto <77185420+Shannto@users.noreply.github.com>
2024-06-07 21:34:54 +0600

    minor change

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -78,7 +78,6 @@ export default function MainComponent(){
         const response = await axios.get("/api/getClientIp");
           const clientIp = response.data.ip;
           console.log(clientIp)
-        setSearchValue(clientIp);
          fetchLocation(clientIp);
       } catch (error) {
         console.error("Error fetching client IP:", error);
